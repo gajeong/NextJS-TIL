f22d5154aebee1f1c1428df20e2c28039c4f4b88
gajeong <rlarkwjd127@gmail.com>
2023-06-16 18:20:41 +0900

    SSG 렌더링

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { notFound } from 'next/navigation'
 import { getProduct, getProducts } from '@/service/products'
+
 type Props = {
   params: {
     slug: string
   }
 }
-export default function Pants({ params: { slug } }: Props) {
-  const product = getProduct(slug)
+
+export default async function ProductPage({
+  params: { slug },
+}: Props) {
+  const product = await getProduct(slug)
   if (!product) {
     notFound()
   }
-  return <div>{product}제품 설명 페이지</div>
+  return <div>{product.name}제품 설명 페이지</div>
 }
 
-export function generateStaticParams() {
-  const products = getProducts()
+export async function generateStaticParams() {
+  const products = await getProducts()
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }))
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,16 +2,16 @@ import { getProducts } from '@/service/products'
 import Link from 'next/link'
 import React from 'react'
 
-export default function ProductsPage() {
-  const products = getProducts()
+export default async function ProductsPage() {
+  const products = await getProducts()
   return (
     <>
       <h1>제품 소개페이지</h1>
       <ul>
         {products.map((product, idx) => (
           <li key={idx}>
-            <Link href={`products/${product}`}>
-              {product}
+            <Link href={`products/${product.id}`}>
+              {product.name}
             </Link>
           </li>
         ))}
